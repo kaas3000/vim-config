ccd3e895e24abd06f4b4ce010a0fdaf41e5fb82f
Kees Meliefste <meli0018@hz.nl>
2017-01-06 16:13:18 +0100

    Add config to .vimrc during installation

--- install.sh
+++ install.sh
@@ -3,3 +3,15 @@
 # install plugin manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Add current file to vim config
+define(){ IFS='\n' read -r -d '' ${1} || true; }
+define LOADCONFIGSCRIPT <<EOF
+
+
+" Load custom config
+set runtimepath+=$PWD
+source $PWD/vimrc.vim
+EOF
+
+echo "$LOADCONFIGSCRIPT" >> ~/.vimrc
